49ea1f88c70ecd001c01c2dfe88c11860a3e38d6
Bhavana Sivashankar <bahavanashankar28@gmail.com>
2020-07-12 12:39:58 +0530

    addded hungry promt

--- Hungry.py
+++ Hungry.py
@@ -1,3 +1,7 @@
 Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print ("I'm hungry")
+I'm hungry
+>>> hungry=input("are you hungry")
+are you hungry
+>>> 
